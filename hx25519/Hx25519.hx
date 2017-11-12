c8a20c347add85323a2498c151c2a23ae432c108
Anton Nesterov <ntrf@znscode.net>
2017-11-12 21:18:56 +0300

    Initial commit

---
+++ hx25519/Hx25519.hx
@@ -0,0 +1,520 @@
+package hx25519;
+
+import haxe.Int32;
+import haxe.io.Bytes;
+
+@:forward
+abstract Vec(haxe.ds.Vector< Int32 >)
+{
+	public function new() {
+		this = new haxe.ds.Vector(32);
+	}
+
+	public static function make_zero()
+	{
+		var r = new Vec();
+		for (i in 0 ... 32) r.setV(i, 0);
+		return r;
+	}
+
+	public static function make_one()
+	{
+		var r = new Vec();
+		for (i in 1 ... 32) r.setV(i, 0);
+		r.setV(0, 1);
+		return r;
+	}
+
+	public static function make_a24()
+	{
+		//0x1DB41;
+
+		var r = new Vec();
+		for (i in 1 ... 32) r.setV(i, 0);
+		r.set(0, 0x41);
+		r.set(1, 0xDB);
+		r.set(2, 0x01);
+		return r;
+	}
+
+	inline public function getV(i : Int) : Int32 return this.get(i);
+	inline public function setV(i : Int, v : Int32) { this.set(i, v); }
+
+	public function copy(x : Vec) {
+		for (i in 0 ... 32) setV(i, x.getV(i));
+	}
+
+	public function small(x : Int) {
+		for (i in 1 ... 32) this.set(i, 0);
+		this.set(0, x);
+
+		vnorm();
+	}
+
+	// Functions bellow assume `this` will store the result
+
+	//### need to unroll these loops!
+#if 1
+	public function vadd(a : Vec, b : Vec) {
+		for (i in 0 ... 32)
+			this.set(i, a.get(i) + b.get(i));
+	}
+
+	public function vsub(a : Vec, b : Vec) {
+		for (i in 0 ... 32)
+			this.set(i, a.get(i) - b.get(i));
+	}
+#else
+	public function vadd(a : Vec, b : Vec) {
+		var v : Int32 = 0;
+		var j = 0;
+		while (j < 31) {
+			v += a.get(j) + b.get(j);
+			this.set(j, v & 255);
+			v >>>= 8;
+			j += 1;
+		}
+		v += a.getV(31) + b.getV(31);
+		setV(31, v);
+	}
+
+	public function vsub(a : Vec, b : Vec) {
+		var v : Int32 = 0x8;
+		var j = 0;
+		while (j < 31) {
+			v = (0x7F8 + v) +  a.get(j) - b.get(j);
+			this.set(j, v & 0xFF);
+			v >>= 8;
+			j += 1;
+		}
+		v += 0x7F8 + a.getV(31) - b.getV(31);
+		setV(31, v & 0xFF);
+	}
+#end
+	public function vmult(a : Vec, b : Vec) {
+#if 1
+		var v : Int32;
+		for (i in 0 ... 32) {
+			v = 0;
+			var j = 0;
+			while (j <= i) {
+				v += a.getV(j) * b.getV(i - j);
+				++j;
+			}
+			j = i + 1;
+			while (j < 32) {
+				v += 38 * a.getV(j) * b.getV(i + 32 - j);
+				++j;
+			}
+			setV(i, v);
+		}
+#else
+		var v : Int32;
+		var t = new haxe.ds.Vector< Int >(63);
+		for (i in 0 ... 63) t[i] = 0;
+		for (i in 0 ... 32) {
+			for (j in 0 ... 32) {
+				t.set(i + j, t.get(i + j) + a.get(i) * b.get(j));
+			}
+		}
+
+		for (i in 0 ... 32) {
+			this.set(i, t.get(i) + 38 * t.get(i + 16));
+		}
+#end
+		vnorm();
+		vnorm();
+	}
+
+	public function vsqr(a : Vec) { vmult(a, a); }
+
+	static var _a24 = Vec.make_a24();
+
+	public function vmult_121665(a : Vec)
+	{
+#if 0
+		var v : Int32 = 0;
+		for (j in 0 ... 31) {
+			v += 121665 * a.getV(j);
+			setV(j, v & 255);
+			v >>>= 8;
+		}
+		v += 121665 * a.getV(31);
+		setV(31, v & 127);
+
+		v = 19 * (v >>> 7);
+		for (j in 0 ... 31) {
+			v += getV(j);
+			setV(j, v & 255);
+			v >>>= 8;
+		}
+
+		v += getV(31);
+		setV(31, v);
+#else
+		vmult(a, _a24);
+#end
+
+		vnorm();
+		vnorm();
+	}
+
+	public function vnorm()
+	{
+		var v : Int32 = 0;
+#if 1
+		for (j in 0 ... 31) {
+			v = getV(j) + (1 << 8);
+			var c = v >> 8;
+
+			setV(j + 1, getV(j + 1) + (c - 1));
+			setV(j, v - (c << 8));
+		}
+
+		v = getV(31) + (1 << 8);
+		var c = v >> 8;
+		setV(0, getV(0) + 38 * (c - 1));
+		setV(31, v - (c << 8));
+#else
+		for (j in 0 ... 31) {
+			v += getV(j);
+			setV(j, v & 255);
+			v >>= 8;
+		}
+		v += getV(31);
+		setV(31, v & 127);
+
+		v = 19 * (v >>> 7);
+		for (j in 0 ... 31) {
+			v += getV(j);
+			setV(j, v & 255);
+			v >>= 8;
+		}
+
+		v += getV(31);
+		setV(31, v);
+#end
+	}
+
+	public function freeze(temp: Vec) {
+		vnorm();
+
+		var v : Int32 = this.get(31);
+		var x = v >>> 7;
+		this.set(31, v & 0x7f);
+		v = x * 19;
+		for (j in 0 ... 31) {
+			v += getV(j);
+			this.set(j, v & 255);
+			v >>>= 8;
+		}
+		v += getV(31);
+		setV(31, v);
+	}
+}
+
+
+/**
+ *  Curve25519 DH key exchange algorithm.
+ *  
+ *  This implementation is designed to be fast and secure against
+ *  side channel attacks. This implementation does not include signature 
+ *  scheme. It can however be extended to EC
+ */
+class Hx25519
+{
+	// Part of the ladder for doubling
+	//  x + z     x - z
+	//    |         |
+	//   ^2        ^2
+	//    |---. .---|
+	//    |    X    |
+	//   (*)<-' :->(-) ---.
+	//    |     |   |     |
+	//    |     |  (*) <--|-- a24
+	//    |     |   |     |
+	//    |     '->(+)    |
+	//    |         |     |
+	//    |        (*) <--'
+	//    |         |
+	//    v         v
+	// 
+	//    X         Z
+	//
+	// - this function destroys contents of tx, ty
+	static function pt_double(rx : Vec, rz : Vec, ax : Vec, az : Vec, tx : Vec, tz : Vec)
+	{
+		//### there should be more efficient formula for squaring
+		tx.vmult(ax, ax);
+		tz.vmult(az, az);
+
+		rx.vmult(tx, tz);
+
+		tz.vsub(tx, tz);
+		rz.vmult_121665(tz);
+		tx.vadd(tx, rz);
+		rz.vmult(tx, tz);
+	}
+
+	// Part of the ladder for addition
+	//           x1 + z1   x1 - z1
+	//              |         |
+	// x2 - z2 --> (*)       (*) <-- x2 + z2
+	//              |---. .---|
+	//              |    X    |
+	//             (+)<-' '->(-)
+	//              |         | 
+	//             ^2        ^2
+	//              |         |
+	//              |        (*) <-- dx
+	//              |         |
+	//              v         v
+	//           
+	//              X         Z
+	//
+	// - this function destroys contents of ax & az
+	static function pt_add(rx : Vec, rz : Vec, ax : Vec, az : Vec, bx : Vec, bz : Vec, dx : Vec)
+	{
+		//### there should be more efficient formula for squaring
+		rx.vmult(ax, bz);
+		rz.vmult(az, bx);
+
+		ax.vadd(rx, rz);
+		az.vsub(rx, rz);
+
+		rx.vmult(ax, ax);
+		ax.vmult(az, az);
+
+		rz.vmult(ax, dx);
+	}
+
+	// Top part of each side of the ladder
+	//
+	// rx = ax + az
+	// ry = ax - az
+	static function pt_diffs(rx : Vec, rz: Vec, ax : Vec, az : Vec)
+	{
+		rx.vadd(ax, az);
+		rz.vsub(ax, az);
+	}
+
+	//------------------------------------------
+	static function cond_swap(x1 : Vec, x2 : Vec, b : Int32)
+	{
+		var mn : Int32 = (b - 1) & 0xFFFFFFFF; // b=1 => 0 / b=0 => ~0
+		var mp : Int32 = (~mn) & 0xFFFFFFFF;
+
+		for (i in 0 ... 32) {
+			var a = x1.getV(i);
+			var b = x2.getV(i);
+
+			var tx = (a ^ b) & mp;
+			
+			x1.setV(i, a ^ tx);
+			x2.setV(i, b ^ tx);
+		}
+	}
+
+	static function recip(z : Vec, t0 : Vec, t1 : Vec, t2 : Vec, t3 : Vec, t4 : Vec) {
+
+		/* the chain for z^(2^255-21) is straight from djb's implementation */
+		t1.vsqr(z);				//  2 == 2 * 1
+		t2.vsqr(t1);			//  4 == 2 * 2
+		t0.vsqr(t2);			//  8 == 2 * 4
+		t2.vmult(t0, z);		//  9 == 8 + 1
+		t0.vmult(t2, t1);		// 11 == 9 + 2
+		t1.vsqr(t0);			// 22 == 2 * 11
+		t3.vmult(t1, t2);		// 31 == 22 + 9 == 2^5   - 2^0
+		t1.vsqr(t3);			// 2^6   - 2^1
+		t2.vsqr(t1);			// 2^7   - 2^2
+		t1.vsqr(t2);			// 2^8   - 2^3
+		t2.vsqr(t1);			// 2^9   - 2^4
+		t1.vsqr(t2);			// 2^10  - 2^5
+		t2.vmult(t1, t3);		// 2^10  - 2^0
+		t1.vsqr(t2);			// 2^11  - 2^1
+		t3.vsqr(t1);			// 2^12  - 2^2
+
+		for (i in 1 ... 5) {
+			t1.vsqr(t3);
+			t3.vsqr(t1);
+		}						// 2^20  - 2^10
+		t1.vmult(t3, t2);		// 2^20  - 2^0	
+		t3.vsqr(t1);			// 2^21  - 2^1
+		t4.vsqr(t3);			// 2^22  - 2^2
+		for (i in 1 ... 10) {
+			t3.vsqr(t4);
+			t4.vsqr(t3);
+		}						// 2^40  - 2^20
+		t3.vmult(t4, t1);		// 2^40  - 2^0
+		for (i in 0 ... 5) {
+			t1.vsqr(t3);
+			t3.vsqr(t1);
+		}						// 2^50  - 2^10
+		t1.vmult(t3, t2);		// 2^50  - 2^0
+		t2.vsqr(t1);			// 2^51  - 2^1
+		t3.vsqr(t2);			// 2^52  - 2^2
+		for (i in 1 ... 25) {
+			t2.vsqr(t3);
+			t3.vsqr(t2);
+		}						// 2^100 - 2^50
+		t2.vmult(t3, t1);		// 2^100 - 2^0
+		t3.vsqr(t2);			// 2^101 - 2^1
+		t4.vsqr(t3);			// 2^102 - 2^2
+		for (i in 1 ... 50) {
+			t3.vsqr(t4);
+			t4.vsqr(t3);
+		}						// 2^200 - 2^100
+		t3.vmult(t4, t2);		// 2^200 - 2^0
+		for (i in 0 ... 25) {
+			t4.vsqr(t3);
+			t3.vsqr(t4);
+		}						// 2^250 - 2^50
+		t2.vmult(t3, t1);		// 2^250 - 2^0
+		t1.vsqr(t2);			// 2^251 - 2^1
+		t2.vsqr(t1);			// 2^252 - 2^2
+
+		t1.vsqr(t2);			// 2^253 - 2^3
+		t2.vsqr(t1);			// 2^254 - 2^4
+		t1.vsqr(t2);			// 2^255 - 2^5
+		z.vmult(t1, t0);		// 2^255 - 21
+	}
+
+	static function curve25519(result : Bytes, multiplier : Bytes, base : Bytes)
+	{
+		var e = Bytes.alloc(32);
+		e.blit(0, multiplier, 0, 32);
+
+		e.set(0, e.get(0) & 248);
+		e.set(31, (e.get(31) & 127) | 64);
+		
+		var px = Vec.make_one();
+		var pz = Vec.make_zero();
+		var qx = new Vec();
+		var qz = Vec.make_one();
+
+		// expand base into Qx
+		if (base == null) {
+			// basepoint for key generator
+			// always x = 9
+			qx.small(9);
+		} else {
+			// unpack bytes into array of int32
+			for (i in 0 ... 32) {
+				qx.setV(i, base.get(i));
+			}
+		}
+
+		var dx = new Vec();
+		dx.copy(qx);
+
+		var t0 = new Vec();
+		var t1 = new Vec();
+		var t2 = new Vec();
+		var t3 = new Vec();
+
+
+		// Montgomery ladder
+		//
+		// R0 = Zero (x = 0, z = 1)
+		// R1 = P    (x = e, z = 1)
+
+		// Main loop
+		var y = e.length - 1;
+		while (y >= 0) {
+			var yy = e.get(y);
+
+			var bm = 8;
+			while ((--bm) >= 0) {
+				var b = (yy >> 7) & 1;
+				
+				yy <<= 1;
+
+				//### make this cond-swap later
+				// depending on bit we need to do one of two operations
+				//	if (b == 0) {
+				//		Q = pt_add(P, Q)
+				//		P = pt_double(P)
+				//	} else {
+				//		P = pt_add(P, Q)
+				//		Q = pt_double(Q)
+				//	}
+
+				cond_swap(px, qx, b);
+				cond_swap(pz, qz, b);
+
+				pt_diffs(t0, t1, qx, qz);
+				pt_diffs(t2, t3, px, pz);
+
+				if (y > 32) {
+					traceHex("t2", t2);
+					traceHex("t3", t3);
+					traceHex("t0", t0);
+					traceHex("t1", t1);
+				}
+
+				pt_add(qx, qz, t0, t1, t2, t3, dx);
+				pt_double(px, pz, t2, t3, t0, t1);
+
+				if (y > 32) {
+					traceHex("px", px);
+					traceHex("pz", pz);
+					traceHex("qx", qx);
+					traceHex("qz", qz);
+					trace("----");
+				}
+
+				cond_swap(px, qx, b);
+				cond_swap(pz, qz, b);
+			}
+
+			--y;
+		}
+
+		traceHex("px", px);
+		traceHex("pz", pz);
+		traceHex("qx", qx);
+		traceHex("qz", qz);
+
+		// finalize
+		recip(pz, t0, t1, t2, t3, qz); // compute inverse of z coordinate
+		qx.vmult(px, pz); // scale x with z
+		qx.freeze(px);
+
+		for (i in 0 ... 32) {
+			result.set(i, qx.getV(i));
+		}
+	}
+
+	public static function genKeypair(secretKey : Bytes)
+	{
+		var sharedKey = haxe.io.Bytes.alloc(32);
+
+		curve25519(sharedKey, secretKey, null);
+		return sharedKey;
+	}
+
+	public static function combineKeys(secretKey : Bytes, publicKey : Bytes)
+	{
+		if (secretKey != null && secretKey.length != 32) throw "Bad secretKey length";
+		if (publicKey != null && publicKey.length != 32) throw "Bad publicKey length";
+
+		var sharedKey = haxe.io.Bytes.alloc(32);
+
+		curve25519(sharedKey, secretKey, publicKey);
+		return sharedKey;
+	}
+
+	static inline function traceHex(name : String, x : Vec)
+	{
+#if 0
+		var s = new StringBuf();
+
+		for (i in 0 ... x.length) {
+			s.add(StringTools.hex(x.get(i), 2));
+			s.add(" ");
+		}
+
+		trace('$name => ${s.toString()}');
+#end
+	}
+}
---
+++ tests/TestCurve.hx
@@ -0,0 +1,76 @@
+package tests;
+
+import haxe.io.Bytes;
+
+class TestCurve
+{
+	static function printKey(name : String, x : Bytes)
+	{
+		var s = new StringBuf();
+
+		for (i in 0 ... x.length) {
+			s.add(StringTools.hex(x.get(i), 2));
+			s.add(" ");
+		}
+
+		trace('Key $name => ${s.toString()}');
+	}
+
+	static public function main()
+	{
+		var key1 = Bytes.alloc(32);
+		var key2 = Bytes.alloc(32);
+
+		var key1_data = 
+		[0x6A,0x2C,0xB9,0x1D, 0xA5,0xFB,0x77,0xB1,
+		0x2A,0x99,0xC0,0xEB, 0x87,0x2F,0x4C,0xDF,
+        0x45,0x66,0xB2,0x51, 0x72,0xC1,0x16,0x3C,
+		0x7D,0xA5,0x18,0x73, 0x0A,0x6D,0x07,0x70];
+
+		var key2_data = 
+		[0x6B,0xE0,0x88,0xFF,0x27,0x8B,0x2F,0x1C,0xFD,0xB6,0x18,0x26,0x29,0xB1,0x3B,0x6F,
+        0xE6,0x0E,0x80,0x83,0x8B,0x7F,0xE1,0x79,0x4B,0x8A,0x4A,0x62,0x7E,0x08,0xAB,0x58];
+
+		var index = 0;
+
+		while (true) {
+
+			index++;
+
+			for (i in 0 ... 32) {
+				key1.set(i, Std.random(256));
+				key2.set(i, Std.random(256));
+				//key1.set(i, key1_data[31 - i]);
+				//key2.set(i, key2_data[31 - i]);
+			}
+
+			var pk1 = hx25519.Hx25519.genKeypair(key1);
+			var pk2 = hx25519.Hx25519.genKeypair(key2);
+
+			var sk1 = hx25519.Hx25519.combineKeys(key1, pk2);
+			var sk2 = hx25519.Hx25519.combineKeys(key2, pk1);
+
+			var i = sk1.compare(sk2);
+
+			if (i != 0) {
+				printKey("Private key 1", key1);
+				printKey("Private key 2", key2);
+
+				printKey("Public key 1", pk1);
+				printKey("Public key 2", pk2);
+
+				printKey("Shared key 1", sk1);
+				printKey("Shared key 2", sk2);
+
+				trace('attempt #$index : MISSMATCH');
+				break;
+			} else {
+				//trace("-------- OK ---------");
+			}
+
+			trace('attempt #$index : OK');
+
+			Sys.sleep(0.02);
+		}
+	}
+}
